4f4d4741524887b494a2435777b54e9c1b2aae3d
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-06-26 21:05:16 +0600

    setup application and adding shadcn-ui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-bold pt-10">To Do Application</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
